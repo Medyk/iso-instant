0ea3f453987b6133d93eb6f6e0b622c8ab325ceb
Maciej Medyński <maciej@medynski.waw.pl>
2019-04-24 14:02:26 +0200

    Initial release

---
+++ src/main/java/pl/waw/medynski/util/ISOInstant.java
@@ -0,0 +1,125 @@
+/**
+ * History:
+ *
+ * 1 (2019-04-16) - Initial release.
+ */
+package pl.waw.medynski.util;
+
+
+
+
+/**
+ * Imports.
+ */
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.regex.Pattern;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+import org.joda.time.format.DateTimeParser;
+
+
+
+
+/**
+ * ISOInstant class.
+ *
+ * @author Maciej Medyński
+ * @version 1
+ */
+public class ISOInstant
+{
+    /**
+     * Parse string according to ISO8601 date-time with timezone format.
+     * Format: (YYYY[-]MM[-]DD)'T'(HH[:]MM[:]SS[.SSS])(Z|±00[:00])
+     * where [] - optional, () - group (date/time/timezone), | - or, 'x' - literal
+     * For details see {@link #ISO_PATTERN}.
+     * 
+     * @param text
+     * @return 
+     * @see #ISO_PATTERN
+     */
+    public static Instant parse(final String text)
+    {
+        if (null == text) return null;
+        final String dateTime = text.trim();
+        if (!ISO_PATTERN.matcher(dateTime).matches())
+        {
+            throw new DateTimeException("Failed to match ISO8601 full DateTime with Timezone format");
+        }
+        final long epochMillis = org.joda.time.Instant.parse(dateTime, DATETIME_FORMATTER).getMillis(); // to epoch UTC
+        return Instant.ofEpochMilli(epochMillis); // from epoch UTC
+    }
+    
+    
+    /**
+     * ISO-8601 supported variants pattern.
+     */
+    private static final Pattern ISO_PATTERN = Pattern
+            .compile("^((\\d{8})|(\\d{4}-\\d{2}-\\d{2}))T((\\d{6})|(\\d{2}:\\d{2}:\\d{2}))(\\.\\d{3})?(([+-]\\d{2}(:?\\d{2})?)|Z)$");
+    
+    
+    /**
+     * Date separator parser.
+     */
+    private static final DateTimeParser DATE_SEPARATOR_PARSER = new DateTimeFormatterBuilder()
+            .appendLiteral('-')
+            .toParser();
+    
+    
+    /**
+     * Time separator parser.
+     */
+    private static final DateTimeParser TIME_SEPARATOR_PARSER = new DateTimeFormatterBuilder()
+            .appendLiteral(':')
+            .toParser();
+    
+    
+    /**
+     * Decimal point parser (took from Joda).
+     */
+    private static final DateTimeParser DECIMAL_POINT_PARSER = new DateTimeFormatterBuilder()
+            .append(null, new DateTimeParser[]
+            {
+                new DateTimeFormatterBuilder().appendLiteral('.').toParser(),
+                new DateTimeFormatterBuilder().appendLiteral(',').toParser()
+            })
+            .toParser();
+    
+    
+    /**
+     * Milliseconds parser.
+     */
+    private static final DateTimeParser MILLIS_PARSER = new DateTimeFormatterBuilder()
+            .append(DECIMAL_POINT_PARSER)
+            .appendFractionOfSecond(1, 9)
+            .toParser();
+    
+    
+    /**
+     * Date-time parser.
+     */
+    private static final DateTimeParser DATETIME_PARSER = new DateTimeFormatterBuilder()
+            .appendYear(4, 4)
+            .appendOptional(DATE_SEPARATOR_PARSER)
+            .appendMonthOfYear(2)
+            .appendOptional(DATE_SEPARATOR_PARSER)
+            .appendDayOfMonth(2)
+            .appendLiteral('T')
+            .appendHourOfDay(2)
+            .appendOptional(TIME_SEPARATOR_PARSER)
+            .appendMinuteOfHour(2)
+            .appendOptional(TIME_SEPARATOR_PARSER)
+            .appendSecondOfMinute(2)
+            .appendOptional(MILLIS_PARSER)
+            .appendTimeZoneOffset("Z", true, 2, 4)
+            .toParser();
+    
+    
+    /**
+     * Date-time formatter.
+     */
+    private static final DateTimeFormatter DATETIME_FORMATTER = new DateTimeFormatterBuilder()
+            .append(null, new DateTimeParser[] { DATETIME_PARSER })
+            .toFormatter();
+}
---
+++ src/test/java/pl/waw/medynski/util/ISOInstantTest.java
@@ -0,0 +1,108 @@
+/**
+ * History:
+ *
+ * 1 (2019-04-16) - Initial release.
+ */
+package pl.waw.medynski.util;
+
+
+
+
+/**
+ * Imports.
+ */
+import java.time.Instant;
+import java.util.Date;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+
+/**
+ * ISOInstantTest class.
+ * 
+ * @author Maciej Medyński
+ * @version 1
+ */
+public class ISOInstantTest
+{
+    @Test
+    public void parse_full()
+    {
+        Instant d1 = ISOInstant.parse("1970-01-01T00:00:01.000Z");
+        Instant d2 = ISOInstant.parse("1970-01-01T00:00:01Z");
+        Instant d3 = ISOInstant.parse("1970-01-01T00:00:01.000+00:00");
+        Instant d4 = ISOInstant.parse("1970-01-01T00:00:01+00:00");
+        Instant d5 = ISOInstant.parse("1970-01-01T01:00:01+01:00");
+        Instant d6 = ISOInstant.parse("1970-01-01T05:30:01.000+05:30");
+        Date date = new Date(1000);
+        
+        Assertions.assertEquals(d1, d2);
+        Assertions.assertEquals(d2, d3);
+        Assertions.assertEquals(d3, d4);
+        Assertions.assertEquals(d4, d5);
+        Assertions.assertEquals(d5, d6);
+        Assertions.assertEquals(d1.toEpochMilli(), date.getTime());
+    }
+    
+    
+    @Test
+    public void parse_dateshort()
+    {
+        Instant d1 = ISOInstant.parse("19700101T00:00:01.000Z");
+        Instant d2 = ISOInstant.parse("19700101T00:00:01Z");
+        Instant d3 = ISOInstant.parse("19700101T00:00:01.000+00:00");
+        Instant d4 = ISOInstant.parse("19700101T00:00:01+00:00");
+        Instant d5 = ISOInstant.parse("19700101T01:00:01+01:00");
+        Instant d6 = ISOInstant.parse("19700101T05:30:01.000+05:30");
+        Date date = new Date(1000);
+        
+        Assertions.assertEquals(d1, d2);
+        Assertions.assertEquals(d2, d3);
+        Assertions.assertEquals(d3, d4);
+        Assertions.assertEquals(d4, d5);
+        Assertions.assertEquals(d5, d6);
+        Assertions.assertEquals(d1.toEpochMilli(), date.getTime());
+    }
+    
+    
+    @Test
+    public void parse_timeshort()
+    {
+        Instant d1 = ISOInstant.parse("1970-01-01T000001.000Z");
+        Instant d2 = ISOInstant.parse("1970-01-01T000001Z");
+        Instant d3 = ISOInstant.parse("1970-01-01T000001.000+00:00");
+        Instant d4 = ISOInstant.parse("1970-01-01T000001+00:00");
+        Instant d5 = ISOInstant.parse("1970-01-01T010001+01:00");
+        Instant d6 = ISOInstant.parse("1970-01-01T053001.000+05:30");
+        Date date = new Date(1000);
+        
+        Assertions.assertEquals(d1, d2);
+        Assertions.assertEquals(d2, d3);
+        Assertions.assertEquals(d3, d4);
+        Assertions.assertEquals(d4, d5);
+        Assertions.assertEquals(d5, d6);
+        Assertions.assertEquals(d1.toEpochMilli(), date.getTime());
+    }
+    
+    
+    @Test
+    public void parse_short()
+    {
+        Instant d1 = ISOInstant.parse("19700101T000001.000Z");
+        Instant d2 = ISOInstant.parse("19700101T000001Z");
+        Instant d3 = ISOInstant.parse("19700101T000001.000+00:00");
+        Instant d4 = ISOInstant.parse("19700101T000001+00:00");
+        Instant d5 = ISOInstant.parse("19700101T010001+01:00");
+        Instant d6 = ISOInstant.parse("19700101T053001.000+05:30");
+        Date date = new Date(1000);
+        
+        Assertions.assertEquals(d1, d2);
+        Assertions.assertEquals(d2, d3);
+        Assertions.assertEquals(d3, d4);
+        Assertions.assertEquals(d4, d5);
+        Assertions.assertEquals(d5, d6);
+        Assertions.assertEquals(d1.toEpochMilli(), date.getTime());
+    }
+}
